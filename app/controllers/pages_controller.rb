d8568ac2c6f557c35b3dbd681ad8f18c5fab809c
Alexis Velluet <alexis.velluet@outlook.com>
2023-06-07 10:47:20 +0200

    routes and views for basic pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :gear, :safety, :spots, :learning ]
 
   def home
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get "/gear", to: "pages#gear"
+  get "/safety", to: "pages#safety"
+  get "/spots", to: "pages#spots"
+  get "/learning", to: "pages#learning"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
