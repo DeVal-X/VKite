77b0d9557f0e66b716994c1fab23d043451bb131
Alexis Velluet <alexis.velluet@outlook.com>
2023-06-27 15:01:16 +0200

    changed routes for @shops to be recognised by our view

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,6 +1,7 @@
 class ShopsController < ApplicationController
+  skip_before_action :authenticate_user!
+  
   def index
     @shops = Shop.all
-    render "gear"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get "/gear", to: "pages#gear"
   get "/safety", to: "pages#safety"
   get "/spots", to: "pages#spots"
   get "/learn", to: "pages#learn"
   get "/about", to: "pages#about"
 
-  resources :shops
+  get "shops", to: "shops#index", as: :gear
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
